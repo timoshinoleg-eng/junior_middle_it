452599c39ba1f038f0f66246fcc8dd3f9470346b
timoshinoleg-eng <timoshin.oleg@gmail.com>
2026-09-11 15:55:14 +0300

    fix(webhook): expose direct language command

--- localized_webhook_runtime.py
+++ localized_webhook_runtime.py
@@ -3,8 +3,10 @@ import interactive_webhook_runtime as base
 
 from telegram import CallbackQuery
 from telegram.error import TelegramError
+from telegram.ext import CommandHandler
 
 _original_runtime = base._runtime
+_original_build_runtime = base.build_runtime
 _original_callback_answer = CallbackQuery.answer
 
 
@@ -32,6 +34,15 @@ def _localized_runtime():
 
 
 base._runtime = _localized_runtime
+
+
+def _localized_build_runtime():
+    application, db, job_bot = _original_build_runtime()
+    application.add_handler(CommandHandler("language", job_bot.cmd_language))
+    return application, db, job_bot
+
+
+base.build_runtime = _localized_build_runtime
 
 MAX_UPDATE_BYTES = base.MAX_UPDATE_BYTES
 process_update_payload = base.process_update_payload
